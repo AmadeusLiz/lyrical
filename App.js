d3de3f5e63c10805c26481a3fab45193e74a317f
Lizzie Maradiaga <lg_castrom@unicah.edu>
2020-11-03 07:25:40 -0600

    Creación del proyecto, esqueleto base

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SongListScreen from './src/screens/SongListScreen';
+
+export default function App() {
+  return <></>;
+}
+
+
---
+++ src/screens/SongListScreen.js
@@ -0,0 +1,9 @@
+//Importar módulos necesarios
+import React from "react";
+
+//Variable que contiene la pantalla
+const SongListScreen () => {
+    return <></>;
+}
+
+export default SongListScreen;
